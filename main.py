c47b0cd7a5ad55d040678361785e936c1db52f77
pog <pooriagolestanipg@gmail.com>
2024-03-16 14:18:27 +0330

    some bugs were fixed but now results varie for each time i train the model: sometimes cost remains the same during training and paramters dont change a lot sometimes cost decreses during training but somewhere in traininng some paramters gets nan and results to geting nan prediction and even when none of above happens model is overfiting the training data

--- main.py
+++ main.py
@@ -24,8 +24,48 @@ test_data = norm_df[(norm_df.index % 3 == 2) & (norm_df.index % 2 == 0)]
 evaluation_data = norm_df[(norm_df.index % 3 == 2) & (norm_df.index % 2 == 1)]
 
 layers = [[len(norm_df.columns) - 1], [10, nn.relu], [1, nn.sigmoid]]
-M = nn.NN_model(layers, 3e-2, 10000, nn.CE)
+M = nn.NN_model(layers, 3e-3, 100, nn.CE)
 
-cad.train(train_data, 40, 20, M)
+cad.train(train_data, 20, 20, M)
+
+train_accuracy_batch = csv_as_dataset.random_sample(train_data, 100)
+c = 0
+for i in range(len(train_accuracy_batch)):
+    out = train_accuracy_batch["output"].iloc[i]
+
+    x = train_accuracy_batch[train_accuracy_batch.columns[:-1]]
+    X = np.array(x.values.tolist()[i]).T
+    X = X.reshape(-1, 1)
+    A, caches = M.forward(X)
+    if(abs(out - A) < 0.5):
+        c += 1
+
+print(f"train accuracy:{c}")
+#################
+test_accuracy_batch = csv_as_dataset.random_sample(test_data, 100)
+
+c = 0
+for i in range(len(test_accuracy_batch)):
+    out = test_accuracy_batch["output"].iloc[i]
+
+    x = test_accuracy_batch[test_accuracy_batch.columns[:-1]]
+    X = np.array(x.values.tolist()[i]).T
+    X = X.reshape(-1, 1)
+    A, caches = M.forward(X)
+    if(abs(out - A) < 0.5):
+        c += 1
+print(f"test accuracy:{c}")
+
+###########
+test_sample = csv_as_dataset.random_sample(test_data, 20)
+for i in range(len(test_sample)):
+    print(f'actual value{test_sample["output"].iloc[i]}')
+
+    x = test_sample[test_sample.columns[:-1]]
+    X = np.array(x.values.tolist()[i]).T
+    X = X.reshape(-1, 1)
+    A, caches = M.forward(X)
+    print(f'prediction{A}')
+    print("..............................")
 
 plot_losses(M.losses)
